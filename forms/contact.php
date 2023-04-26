5705059dd423c7a4ea87db4c3a92405825754520
Daniel Gonzalez Placito <daargonzalezpl@ittepic.edu.mx>
2023-04-25 17:53:24 -0700

    Contact.php fix

--- forms/contact.php
+++ forms/contact.php
@@ -7,21 +7,21 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  // $receiving_email_address = 'daplacito@outlook.com';
+  $receiving_email_address = 'daplacito@outlook.com';
 
-  // if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-  //   include( $php_email_form );
-  // } else {
-  //   die( 'Unable to load the "PHP Email Form" Library!');
-  // }
+   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
+     include( $php_email_form );
+   } else {
+     die( 'Unable to load the "PHP Email Form" Library!');
+   }
 
-  // $contact = new PHP_Email_Form;
-  // $contact->ajax = true;
+   $contact = new PHP_Email_Form;
+   $contact->ajax = true;
   
-  // $contact->to = $receiving_email_address;
-  // $contact->from_name = $_POST['name'];
-  // $contact->from_email = $_POST['email'];
-  // $contact->subject = $_POST['subject'];
+   $contact->to = $receiving_email_address;
+   $contact->from_name = $_POST['name'];
+   $contact->from_email = $_POST['email'];
+   $contact->subject = $_POST['subject'];
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
   /*
@@ -33,26 +33,9 @@
   );
   */
 
-  // $contact->add_message( $_POST['name'], 'From');
-  // $contact->add_message( $_POST['email'], 'Email');
-  // $contact->add_message( $_POST['message'], 'Message', 10);
+  $contact->add_message( $_POST['name'], 'From');
+  $contact->add_message( $_POST['email'], 'Email');
+  $contact->add_message( $_POST['message'], 'Message', 10);
 
-  // echo $contact->send();
-
-
-$nombre = $_POST['name'];
-$email = $_POST['email'];
-$asunto = $_POST['subject'];
-$mensaje = $_POST['message'];
-
-//Como va a llegar el cuerpo del mensaje
-$mensaje = "Este mensaje fue enviado por " . $nombre .",\r\n";
-$mensaje = "Su correo es: " . $email .",\r\n";
-$mensaje = "Mensaje: " . $_POST['mensaje'] .",\r\n";
-$mensaje = "Enviado el " . date('d/m/Y', time());
-
-$para = 'daplacito@outlook.com';
-
-mail($para, $asunto, utf8_decode($mensaje), $header);
-header('Location:index.html');
+  echo $contact->send();
 ?>
